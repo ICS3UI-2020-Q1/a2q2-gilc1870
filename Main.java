7db880e514fb9cb49395c0a027bdaca557517816
gilc1870 <gilc1870@wrdsb.ca>
2020-10-07 16:28:47 +0000

    Determines which integer is biggest

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
-/**
- *
- * @author 
- */
+import java.util.Scanner;
+ // Determines which integer is biggest
+ //Cesar Gil
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // asking for the two integers
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter an integer");
+    int num = input.nextInt();
+    System.out.println("Please enter another integer");
+    int num2 = input.nextInt();
+    // if statement that says which one is bigger
+    if (num > num2){
+      System.out.println("The biggest number is " + num);
+    } else {
+      System.out.println("The biggest number is " + num2);
+    } if (num == num2){
+      System.out.println("There is no biggest integer");
+    }
+
   }
 }
